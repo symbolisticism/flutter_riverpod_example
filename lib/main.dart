943dc560576cfefccffd90661ab0b4fc3fa3a927
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-19 12:13:09 -0700

    finished styling and structure of main screen, as well as implementing the notifier functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_example/providers/numbers_provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: const MyApp(),
-      theme: ThemeData.light(
-        useMaterial3: true,
+    ProviderScope(
+      child: MaterialApp(
+        home: const MyApp(),
+        theme: ThemeData.light(
+          useMaterial3: true,
+        ),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
+
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
   @override
   Widget build(BuildContext context) {
+    final number = ref.watch(numberProvider);
+    final provider = ref.watch(numberProvider.notifier);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Riverpod Example'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: provider.subtract,
+            icon: const Icon(Icons.remove),
+          ),
+          const SizedBox(width: 10),
+          IconButton(
+            onPressed: provider.add,
             icon: const Icon(Icons.add),
           ),
         ],
+        backgroundColor: Colors.green[200],
       ),
-      body: const Center(child: Text('Numbers')),
+      body: Center(
+        child: Text(
+          number.toString(),
+          style: const TextStyle(fontSize: 48),
+        ),
+      ),
     );
   }
 }
