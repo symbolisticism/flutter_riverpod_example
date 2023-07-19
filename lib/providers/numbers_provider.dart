f4176c9a4cb1d7596e5b76fe62e98732b75be69d
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-19 12:23:45 -0700

    added comments where users should implement

--- lib/providers/numbers_provider.dart
+++ lib/providers/numbers_provider.dart
@@ -3,17 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class NumberNotifier extends StateNotifier<int> {
   NumberNotifier() : super(0);
 
-  void add() {
-    state = state + 1;
-  }
+  // add 1 to the current state
 
-  void subtract() {
-    state = state - 1;
-  }
+  // subtract 1 from the current state
 
-  void reset() {
-    state = 0;
-  }
+  // reset the number back to 0
 }
 
 final numberProvider =
