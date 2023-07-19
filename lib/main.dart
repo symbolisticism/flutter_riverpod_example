02112e3675b9915cf57e1df414abb152d87d7844
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-19 12:46:22 -0700

    added comments where users should implement

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,10 @@ import 'package:riverpod_example/providers/numbers_provider.dart';
 void main() {
   runApp(
     // add ProviderScope
-    ProviderScope(
-      child: MaterialApp(
-        home: const MyApp(),
-        theme: ThemeData.light(
-          useMaterial3: true,
-        ),
+    MaterialApp(
+      home: const MyApp(),
+      theme: ThemeData.light(
+        useMaterial3: true,
       ),
     ),
   );
@@ -27,26 +25,25 @@ class _MyAppState extends ConsumerState<MyApp> {
   @override
   Widget build(BuildContext context) {
     // add reference to provider state
-    final number = ref.watch(numberProvider);
+
     // add reference to notifier in order to access functions
-    final provider = ref.watch(numberProvider.notifier);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Riverpod Example'),
         actions: [
           IconButton(
-            onPressed: provider.reset, // call 'subtract' function from notifier
+            onPressed: () {}, // call 'subtract' function from notifier
             icon: const Icon(Icons.restart_alt_rounded),
           ),
           const SizedBox(width: 10),
           IconButton(
-            onPressed:
-                provider.subtract, // call 'subtract' function from notifier
+            onPressed: () {}, // call 'subtract' function from notifier
             icon: const Icon(Icons.remove),
           ),
           const SizedBox(width: 10),
           IconButton(
-            onPressed: provider.add, // call 'add' function from notifier
+            onPressed: () {}, // call 'add' function from notifier
             icon: const Icon(Icons.add),
           ),
         ],
@@ -55,7 +52,7 @@ class _MyAppState extends ConsumerState<MyApp> {
       body: Center(
         child: Text(
           // put your reference to the provider here - don't forget to add .toString() on the end
-          number.toString(),
+          '',
           style: const TextStyle(fontSize: 48),
         ),
       ),
--- lib/providers/numbers_provider.dart
+++ lib/providers/numbers_provider.dart
@@ -4,19 +4,11 @@ class NumberNotifier extends StateNotifier<int> {
   NumberNotifier() : super(0);
 
   // add 1 to the current state
-  void add() {
-    state = state + 1;
-  }
 
   // subtract 1 from the current state
-  void subtract() {
-    state = state - 1;
-  }
 
   // reset the number back to 0
-  void reset() {
-    state = 0;
-  }
+  
 }
 
 final numberProvider =
